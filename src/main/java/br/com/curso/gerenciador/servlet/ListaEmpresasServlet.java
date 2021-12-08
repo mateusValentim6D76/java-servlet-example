5f04f48f71721dc9bcf532ec38e4c1c1b5a9ab0a
Mateus Valentim de Oliveira <mateus.oliveira@keeggo.com>
2021-12-08 14:21:25 -0300

    Adicionando nova url no ListaEmpresasServlet e configurando o web.xml

--- src/main/java/br/com/curso/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/main/java/br/com/curso/gerenciador/servlet/ListaEmpresasServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/listaEmpresas")
+@WebServlet(urlPatterns = {"/listaEmpresas", "/empresas"})
 public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
