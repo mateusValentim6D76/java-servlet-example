c7da1a78bb04995a6eb9d8c1a3d04b6fe9b70895
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2021-12-09 23:25:00 -0300

    Criando pacote Controller

---
+++ src/main/java/br/com/curso/gerenciador/controler/ListaEmpresas.java
@@ -0,0 +1,11 @@
+package br.com.curso.gerenciador.controler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListaEmpresas {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+}
