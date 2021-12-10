770add1bf4fe66c40d40962ed277104ef66c9988
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2021-12-09 23:34:13 -0300

    Implementando método executa, na classe ListaEmpresas, no pacote Controller

--- src/main/java/br/com/curso/gerenciador/controler/ListaEmpresas.java
+++ src/main/java/br/com/curso/gerenciador/controler/ListaEmpresas.java
@@ -1,11 +1,30 @@
 package br.com.curso.gerenciador.controler;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.curso.gerenciador.model.Banco;
+import br.com.curso.gerenciador.model.Empresa;
+
 public class ListaEmpresas {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) {
-		
+	protected void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String paramAcao = request.getParameter("acao");
+
+		if (paramAcao.equals("listando empresas")) {
+			Banco banco = new Banco();
+			List<Empresa> listaEmpresas = banco.getEmpresa();
+
+			request.setAttribute("empresas", listaEmpresas);
+
+			RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+			rd.forward(request, response);
+		}
 	}
 }
---
+++ src/main/java/br/com/curso/gerenciador/servlet/UnicaEntradaServlet.java
@@ -0,0 +1,5 @@
+package br.com.curso.gerenciador.servlet;
+
+public class UnicaEntradaServlet {
+
+}
