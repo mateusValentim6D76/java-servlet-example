a74a6855eedb908a5fbc33afd3520b3c7e914218
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2021-12-16 12:49:27 -0300

    Autorizando o acesso

--- src/main/java/br/com/curso/gerenciador/servlet/UnicaEntradaController.java
+++ src/main/java/br/com/curso/gerenciador/servlet/UnicaEntradaController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.curso.gerenciador.action.Acao;
 import br.com.curso.gerenciador.action.AlteraEmpresa;
@@ -23,6 +24,16 @@ public class UnicaEntradaController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String paramAcao = request.getParameter("acao");
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean eUmaAcaoProtegida = !(paramAcao.equals("login") || paramAcao.equals("LoginForm"));
+		
+		if(eUmaAcaoProtegida && usuarioNaoLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");	
+			//Return para que não tome 429
+			return;
+		}
 
 		String nomeDaClasse = "br.com.curso.gerenciador.action." + paramAcao;
 
